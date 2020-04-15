67e557d0d271e01027c9f931a483ef0531639376
yukseltron <h.yuksel04@gmail.com>
2020-04-15 00:46:03 -0400

    multiple keywords for same response are separated

--- main.js
+++ main.js
@@ -4,15 +4,27 @@ new Vue({
     newKeyword: '',
 	newResponse: '',
     pairs: [],
-    nextId: 0
+    nextId: 0,
+	name: ''
   },
   methods: {
     addNew: function () {
-      this.pairs.push({
-        id: this.nextId++,
-        keyword: this.newKeyword,
-		response: this.newResponse
-	});
+	  if (this.newKeyword.includes(',')) {
+		  listOfKeys = this.newKeyword.split(",");
+		  for (i of listOfKeys) {
+			  this.pairs.push({
+				id: this.nextId++,
+				keyword: i,
+				response: this.newResponse
+				});
+		  }
+	  } else {
+	      this.pairs.push({
+	        id: this.nextId++,
+	        keyword: this.newKeyword,
+			response: this.newResponse
+			});
+	  }
 	  this.newKeyword = '';
 	  this.newResponse = '';
   },
@@ -20,11 +32,12 @@ new Vue({
 		this.pairs.splice(index, 1)
   },
 	exportFile: function () {
+		this.name = this.name + '_config';
 		const data = JSON.stringify(this.pairs)
 	    const blob = new Blob([data], {type: 'text/plain'})
 	    const e = document.createEvent('MouseEvents'),
 	    a = document.createElement('a');
-	    a.download = "test.json";
+	    a.download = this.name + ".json";
 	    a.href = window.URL.createObjectURL(blob);
 	    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
 	    e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
